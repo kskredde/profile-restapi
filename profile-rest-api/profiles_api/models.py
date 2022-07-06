00b72b0786c23037ee08bff9122ce4ca755a8736
koushik_gr <koushik.python37@gmail.com>
2022-07-06 08:58:32 -0400

    Migrations and created super user

--- profile-rest-api/profiles_api/models.py
+++ profile-rest-api/profiles_api/models.py
@@ -8,19 +8,20 @@ class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """ Create a New user Profile"""
+        """Create a new user profile"""
         if not email:
-            raise ValueError("User must have a email address")
-        email = self.normailze_email(email)
-        user = self.model(email=email, name=name)
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_super_user(self, email, name, password):
-        """ Create and save a new superuser with given details"""
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
@@ -29,9 +30,8 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """ Database model for users in the system """
+    """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -43,14 +43,14 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrive full name of user"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        """"Retrive the short name of the user"""
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        """Return string representation of the user"""
+        """Return string representation of user"""
         return self.email
 
