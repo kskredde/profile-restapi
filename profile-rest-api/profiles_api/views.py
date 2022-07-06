a2a4aea38f6a9348b461b66c45c6cbdcfdfb9b36
koushik_gr <koushik.python37@gmail.com>
2022-07-06 16:25:59 -0400

    creted hello-viewset

--- profile-rest-api/profiles_api/views.py
+++ profile-rest-api/profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -14,3 +18,43 @@ class HelloApiView(APIView):
             'Is Mapped manually to urls',
         ]
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(self, request):
+        ''' create a hello message with our name'''
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status = status.HTTP_400_BAD_REQUEST
+                            )
+
+    def put(self, request, pk=None):
+        '''Handle updating an object '''
+        return Response({'method': 'PUT'})
+
+
+    def patch(self, request, pk=None):
+        """Handle Partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """ Delete an object"""
+        return Response({'method': 'DELETE'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    ''' Test API ViewSet'''
+
+    def list(self, request):
+        '''Return a hello message'''
+
+        a_viewset=['Uses actions(list,create,retrieve,update,partial_update)',
+        'Automatically maps to URls using Routers',
+        'Provides more functionality with less code',
+                   ]
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
